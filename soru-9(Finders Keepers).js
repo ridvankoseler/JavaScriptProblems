9a48e3a82df17dc28ab0c2595fa174132fdaeb99
ridvankoseler <ridvankoseler@gmail.com>
2022-06-28 23:37:58 +0300

    uptade3

--- soru-9(Finders Keepers).js
+++ soru-9(Finders Keepers).js
@@ -3,17 +3,15 @@ Bir arr dizisine bakan ve içindeki bir 'doğruluk testini' geçen ilk öğeyi d
 
 function findElement(arr, func) {
   let num = 0;
-    for(let i = 0; i < arr.length ;i++){
-        num = arr[i]
-        if(func(num)) {
-            num += arr[i]
-        }
-    return num;
-
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if (func(num)) {
+      return num;
     }
-    else{
-        return undefined
-    }
-  
+  }
 }
-console.log(findElement([1, 2, 3, 4], (num) => num % 2 === 0));
+console.log(
+  findElement([1, 3, 5, 8, 9, 10], function (num) {
+    return num % 2 === 0;
+  })
+);
